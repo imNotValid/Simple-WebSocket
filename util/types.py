ccbeb3e48b57777a055ab73412f1ac18af7f323a
imNotValid - Amirreza Salahshour <81874460+imNotValid@users.noreply.github.com>
2022-08-30 19:20:43 +0430

    Add files via upload

---
+++ util/types.py
@@ -0,0 +1,28 @@
+from socket import socket
+from typing import Any
+from util.filters import _isit_json
+
+def _decode_message(data: bytes):
+    try:
+        return data.decode().strip()
+
+    except Exception:
+        return False
+
+class Connection:
+    def __init__(self, conn: socket, addr: str, message: Any = None, filter_result: Any = None) -> None:
+        self.conn, self.addr = conn, addr
+        self.ip, self.port = addr
+        self.message, self.json = message, _isit_json(message)
+
+    def send(self, message: str) -> None:
+        type_of_message = type(message)
+        if not type_of_message is bytes:
+            message = message.encode()
+        self.conn.send(message)
+
+    def recieve(self, BufferSize: int = 4096) -> bytes:
+        return _decode_message(self.conn.recv(BufferSize))
+
+    def close(self):
+        self.conn.close()
