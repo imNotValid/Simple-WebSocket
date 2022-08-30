80b4e1db16221d87656f22733e05ddc83c413da3
imNotValid - Amirreza Salahshour <81874460+imNotValid@users.noreply.github.com>
2022-08-30 19:22:30 +0430

    Add files via upload

---
+++ WebSocket.py
@@ -0,0 +1,63 @@
+from socket import AF_INET, SOCK_STREAM, gethostbyname, gethostname, socket
+from threading import Thread
+from util.types import Connection, _decode_message
+
+
+class WebSocket:
+    def __init__(self, host: str = gethostbyname(gethostname()), port: int = 8000, buffer_size: int = 4096) -> None:
+        self.on_message_listener, self.on_connection_listener = [], []
+        self.host, self.port, self.buffer_size = host, port, buffer_size
+
+    def on_new_connection(self, filters=None):
+        def add_new_con(func):
+            self.on_connection_listener.append([func, filters])
+        return add_new_con
+
+    def on_message(self, filters=None):
+        def add_handler(func):
+            self.on_message_listener.append([func, filters])
+        return add_handler
+
+    def __on_update(self, conn: socket, addr):
+        if self.on_connection_listener:
+            data = Connection(conn, addr)
+            for event in self.on_connection_listener:
+                try:
+                    funct, filterss = event
+                    if filterss:
+                        fr = data.filter_result = filterss(data)
+                        if fr:
+                            funct(data)
+                            break
+                    else:
+                        funct(data)
+                except Exception as e:
+                    print(f"{e} - on handler -> {funct.__name__}")
+
+        while True:
+            data = _decode_message(conn.recv(self.buffer_size))
+            if data:
+                data = Connection(conn, addr, data)
+                for event in self.on_message_listener:
+                    try:
+                        func, filters = event
+                        if filters:
+                            data.filter_result = filters(data)
+                            if data.filter_result:
+                                func(data)
+                                break
+                        else:
+                            func(data)
+
+                    except Exception as e:
+                        print(f"{e} - on handler -> {func.__name__}")
+
+    def run(self, host: str = gethostbyname(gethostname()), port: int = 8000, show_ip_after_run=True) -> None:
+        self.host, self.port = host, port
+        self.serv = socket(AF_INET, SOCK_STREAM)
+        self.serv.bind((self.host, self.port)), self.serv.listen()
+        if show_ip_after_run:
+            print(host, port)
+        while True:
+            conn, addr = self.serv.accept()
+            Thread(target=self.__on_update, args=[conn, addr]).start()
---
+++ example.py
@@ -0,0 +1,35 @@
+from re import match
+from WebSocket import WebSocket
+from util.filters import event, regex, filter_ips, create
+from util.types import Connection
+
+app = WebSocket()
+
+# ---- Filter ips - on new connection 
+@app.on_new_connection(filter_ips("192.168.114.226")) # Example
+def on_connection(m: Connection):
+    m.send("hello welcome to our server")
+
+# ---- On event handler
+@app.on_message(event("bing"))
+def event_message(message: Connection):
+    message.send("bong! - from event filter")
+
+# ---- Regex filter
+@app.on_message(regex("ding"))
+def regex_message(message: Connection):
+    message.send("dong! - from regex filter")
+
+# ---- Create filter
+def filter_hello(message: Connection):
+    if match(r"(?i)hello", message.message):
+        return True
+    return False
+
+filter_hello = create(filter_hello)
+
+@app.on_message(filter_hello)
+def message_hello(message: Connection):
+    message.send("Hello! - from filter_hello")
+
+app.run(port=12346)
