a627c65ff00765a9443985ebe6f9199f1ab24099
imNotValid - Amirreza Salahshour <81874460+imNotValid@users.noreply.github.com>
2022-09-04 01:09:23 +0430

    Update WebSocket.py

--- WebSocket.py
+++ WebSocket.py
@@ -5,28 +5,34 @@ from util.types import Connection, _decode_message
 
 class WebSocket:
     def __init__(self, host: str = gethostbyname(gethostname()), port: int = 8000, buffer_size: int = 4096) -> None:
-        self.on_message_listener, self.on_connection_listener = [], []
+        self._on_message_listener, self._on_disconnect_listener, self._on_connection_listener = [], [], []
         self.host, self.port, self.buffer_size = host, port, buffer_size
 
     def on_new_connection(self, filters=None):
-        def add_new_con(func):
-            self.on_connection_listener.append([func, filters])
-        return add_new_con
+        def add_handler(func):
+            self._on_connection_listener.append([func, filters])
+        return add_handler
+
+    def on_disconnect(self, filters=None):
+        def add_handler(func):
+            self._on_disconnect_listener.append([func, filters])
+        return add_handler
 
     def on_message(self, filters=None):
         def add_handler(func):
-            self.on_message_listener.append([func, filters])
+            self._on_message_listener.append([func, filters])
         return add_handler
 
-    def __on_update(self, conn: socket, addr):
-        if self.on_connection_listener:
-            data = Connection(conn, addr)
-            for event in self.on_connection_listener:
+    @staticmethod
+    def _run_listeners(list_of_listeners, *args, **kwargs):
+        if list_of_listeners:
+            data = Connection(*args, **kwargs)
+            for event in list_of_listeners:
                 try:
                     funct, filterss = event
                     if filterss:
-                        data.filter_result = filterss(data)
-                        if data.filter_result:
+                        fr = data.filter_result = filterss(data)
+                        if fr:
                             funct(data)
                             break
                     else:
@@ -34,23 +40,16 @@ class WebSocket:
                 except Exception as e:
                     print(f"{e} - on handler -> {funct.__name__}")
 
+    def _on_update(self, conn: socket, addr):
+        self._run_listeners(self._on_connection_listener, conn, addr)
         while True:
-            data = _decode_message(conn.recv(self.buffer_size))
-            if data:
-                data = Connection(conn, addr, data)
-                for event in self.on_message_listener:
-                    try:
-                        func, filters = event
-                        if filters:
-                            data.filter_result = filters(data)
-                            if data.filter_result:
-                                func(data)
-                                break
-                        else:
-                            func(data)
-
-                    except Exception as e:
-                        print(f"{e} - on handler -> {func.__name__}")
+            try:
+                data = _decode_message(conn.recv(self.buffer_size))
+                if data:
+                    self._run_listeners(self._on_message_listener, conn, addr, data)
+            except OSError:
+                self._run_listeners(self._on_disconnect_listener, conn, addr)
+                break
 
     def run(self, host: str = gethostbyname(gethostname()), port: int = 8000, show_ip_after_run=True) -> None:
         self.host, self.port = host, port
@@ -60,4 +59,4 @@ class WebSocket:
             print(host, port)
         while True:
             conn, addr = self.serv.accept()
-            Thread(target=self.__on_update, args=[conn, addr]).start()
+            Thread(target=self._on_update, args=[conn, addr]).start()
